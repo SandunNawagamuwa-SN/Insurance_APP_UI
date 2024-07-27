e6d84c6f82f35c2b1a6e5d868f38f4b4ae467b16
SandunNawagamuwa-SN <dreamingphoenix916@gmail.com>
2024-07-27 23:06:06 +0530

    Routing Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Layout from './Pages/Layout';
+import Home from './Pages/Home';
+import Register from "./Pages/Auth/Register";
+import Login from "./Pages/Auth/Login";
+import './App.css';
+
 
 export default function App() {
+  const {user} = useContext(AppContext) 
 
   return (
-    <>
-      hello
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
+
+          <Route path='/register' element={user ? <Home />:<Register />}/>
+          <Route path='/login' element={user ? <Home />:<Login />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
