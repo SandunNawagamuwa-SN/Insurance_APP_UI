0ac07bcbb1446b54a1d97f93c32a52fcb96d4dba
SandunNawagamuwa-SN <dreamingphoenix916@gmail.com>
2024-07-29 08:56:45 +0530

    Enhance the functionality of Pagination Component

--- src/Component/Pagination.jsx
+++ src/Component/Pagination.jsx
@@ -26,7 +26,7 @@ export default function Pagination({ pageNumber, setPageNumber, totalPages }) {
                 <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" fillRule="evenodd"></path></svg>
             </button>
             {
-                pages.map((pg, i) => (
+                pages.filter(pg => pg.page !== null).map((pg, i) => (
                     <button key={i} onClick={() => { setPageNumber(pg.page), setCur(pg.page) }} className={`h-8 border-2 border-r-0 border-slate-800
                w-8 ${cur === pg.page && 'bg-slate-800 text-white'}`}>{pg.page}</button>
                 ))
